6f77e1e7ab52a5d09e3d432bf3a5485457b6d488
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-29 00:58:14 +0100

    basic login and register

--- ChatApp.Presentation/Menus/Auth/Login.cs
+++ ChatApp.Presentation/Menus/Auth/Login.cs
@@ -1,8 +1,34 @@
+
 namespace ChatApp.Presentation.Menus;
+using ChatApp.Presentation.Helpers;
+
 public class Login
 {
+    private string Email { get; set; }
+    private string Password { get; set; }
+    private bool isSuccess;
+
     public Login()
     {
-        Console.WriteLine("Login");
+        do{
+            Console.Clear();
+            Email = Reader.ReadEmail("Email: ");
+            Password = Reader.ReadInput("Password: ");
+
+            isSuccess = ValidateUserPassword(Email, Password);
+            Reader.ReadKeyToContinue();
+        } while(!isSuccess);
+    }
+
+    public bool ValidateUserPassword(string email, string password)
+    {
+        //TODO: fetch user eamil and password from database
+        string userPass = "userpass";
+        bool isSuccess = password == userPass;
+        if(isSuccess) 
+            Console.WriteLine("\nLogin successful!");
+        else
+            Console.WriteLine("\nLogin failed!");
+        return isSuccess;
     }
 }
--- ChatApp.Presentation/Menus/Auth/Register.cs
+++ ChatApp.Presentation/Menus/Auth/Register.cs
@@ -1,8 +1,76 @@
 namespace ChatApp.Presentation.Menus;
+using ChatApp.Presentation.Helpers;
     public class Register
     {
+        private string Email { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private string ConfirmPassword { get; set; }
+        private string Captcha { get; set; }
+        private bool isSuccess;
         public Register()
         {
-            Console.WriteLine("Register");
+            do{
+                Console.Clear();
+                Email = Reader.ReadEmail("Email: ");
+                UserName = Reader.ReadInput("Username: ");
+                do{
+                    Password = Reader.ReadInput("Password: ");
+                    ConfirmPassword = Reader.ReadInput("Confirm Password: ");
+                    isSuccess = ConfirmUserPassword(Password, ConfirmPassword);
+                } while(!isSuccess);
+
+                 isSuccess = CreateAndConfirmCaptchaString();
+
+                //TODO: check if that user already is in database, if not create new user
+                Reader.ReadKeyToContinue();
+            } while(!isSuccess);
+        }
+
+        public bool ConfirmUserPassword(string password, string confirmPassword)
+        {
+            bool isSuccess = password == confirmPassword;
+            if(isSuccess) 
+                Console.WriteLine("\nPasswords match!");
+            else
+                Console.WriteLine("\nPasswords do not match!");
+            Reader.ReadKeyToContinue();
+            return isSuccess;
+        }
+
+        public string CreateRandomCaptchaString(int length)
+        {
+            //Console.WriteLine("Captcha: ");
+            //generete random string that contains atleast one number and one character
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            const string nums = "0123456789";
+            var random = new Random();
+
+            var randomChars = new string(Enumerable.Repeat(chars, length - 2)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            var randomNumber = nums[random.Next(nums.Length)];
+            var randomChar = chars[random.Next(chars.Length)];
+
+            var randomString = randomChars.Insert(random.Next(randomChars.Length), randomNumber.ToString())
+                .Insert(random.Next(randomChars.Length), randomChar.ToString());
+
+            return randomString;
+        }
+        public bool CreateAndConfirmCaptchaString()
+        {
+            do{
+                Console.Clear();
+                Captcha = CreateRandomCaptchaString(5);
+                Console.WriteLine($"Captcha: {Captcha}");
+                var input = Reader.ReadInput("Confirm Captcha: ");
+                bool isSuccess = input == Captcha;
+                if(isSuccess) 
+                    Console.WriteLine("\nInputed captcha is correct!");
+                else
+                    Console.WriteLine("\nInputed captcha does not match!, try again.");
+                Reader.ReadKeyToContinue();
+            } while(!isSuccess);
+            return isSuccess;
         }
     }
