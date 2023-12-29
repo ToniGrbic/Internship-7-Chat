6d05fecef771c7e8158598cc954d0cd006c264d0
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-29 20:56:00 +0100

    user queries

---
+++ ChatApp.Domain/Repositories/UsersRepository.cs
@@ -0,0 +1,52 @@
+
+using ChatApp.Data.Entities;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Domain.Enums;
+
+namespace ChatApp.Domain.Repositories;
+
+public class UsersRepository : BaseRepository
+{
+    public UsersRepository(ChatAppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public ResponseResultType Add(Users user)
+    {
+        DbContext.Users.Add(user);
+
+        return SaveChanges();
+    }
+
+    public ResponseResultType Delete(int id)
+    {
+        var userToDelete = DbContext.Users.Find(id);
+        if (userToDelete is null)
+        {
+            return ResponseResultType.NotFound;
+        }
+
+        DbContext.Users.Remove(userToDelete);
+
+        return SaveChanges();
+    }
+
+    public ResponseResultType Update(Users user, int id)
+    {
+        var userToUpdate = DbContext.Users.Find(id);
+        if (userToUpdate is null)
+        {
+            return ResponseResultType.NotFound;
+        }
+
+        userToUpdate.Email = user.Email;
+        userToUpdate.UserName = user.UserName;
+
+        return SaveChanges();
+    }
+
+    public Users? GetByEmail(string email) => DbContext.Users.FirstOrDefault(u => u.Email == email);
+    public ICollection<Users> GetAll() => DbContext.Users.ToList();
+
+    
+}
