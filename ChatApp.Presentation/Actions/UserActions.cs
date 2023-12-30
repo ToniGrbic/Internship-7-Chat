0eb0cf871bed8300e68944cf63bfec8dd5174476
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-30 13:40:46 +0100

    actions for authenticating user

--- ChatApp.Presentation/Actions/UserActions.cs
+++ ChatApp.Presentation/Actions/UserActions.cs
@@ -8,15 +8,53 @@ namespace ChatApp.Presentation.Actions.UserActions;
 public static class UserActions
 {
     private static UsersRepository _userRepository = RepositoryFactory.Create<UsersRepository>();
-    public static void AddUser(string email, string userName, string password)
+    public static void RegisterUser(string email, string userName, string password)
     {
          Users newUser = new(email, password, userName);
          var responseResult = _userRepository.Add(newUser);
    
          if(responseResult == ResponseResultType.Success)
-            Console.WriteLine($"User {userName} added successfully!\n");
+            Console.WriteLine($"User {userName} registered successfully!\n");
          else
-            Console.WriteLine("Failed to add user, no changes saved!");
+            Console.WriteLine("Failed to register user, no changes saved!");
+         Reader.ReadKeyToContinue();
+    }
+
+    public static void DeleteUser(int id)
+    {
+        var responseResult = _userRepository.Delete(id);
+        if(responseResult == ResponseResultType.Success)
+            Console.WriteLine($"User with id {id} deleted successfully!\n");
+        else
+            Console.WriteLine("Failed to delete user, no changes saved!");
+
+        Reader.ReadKeyToContinue();
+    }
+
+    public static Users? GetUserByEmailForRegister(string email)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if(user is not null)
+            Console.WriteLine($"User with email {email} already exists!");
+        return user!;
+    }
+
+    public static Users? GetUserByEmailForLogin(string email)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if(user is null)
+            Console.WriteLine($"User with email {email} does not exist!");
+        return user!;
+    }
+
+
+    public static void UpdateUser(Users user, int id)
+    {
+        var responseResult = _userRepository.Update(user, id);
+        if(responseResult == ResponseResultType.Success)
+            Console.WriteLine($"Changes saved!\n");
+        else
+            Console.WriteLine("Failed to update, no changes saved!\n");
 
         Reader.ReadKeyToContinue();
     }
