5f03c557c441043e8143c3e581f7d8f549cca10c
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-31 15:14:47 +0100

    group chat menu actions

---
+++ ChatApp.Presentation/Actions/GroupUsersActions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Domain.Enums;
+using ChatApp.Domain.Factories;
+using ChatApp.Domain.Repositories;
+
+namespace ChatApp.Presentation.Actions
+{
+    public class GroupUsersActions
+    {
+        private static GroupUsersRepository _groupUsersRepository = RepositoryFactory.Create<GroupUsersRepository>();
+        public static void AddUserToGroup(int userID, int groupID)
+        {
+            var groupUser = new GroupUsers { UserId = userID, GroupId = groupID };
+            var response = _groupUsersRepository.Add(groupID, userID);
+            if (response == ResponseResultType.Success)
+                Console.WriteLine("User added to group");
+            else
+                Console.WriteLine("User not added to group");
+        }
+    }
+}
---
+++ ChatApp.Presentation/Actions/UsersActions.cs
@@ -0,0 +1,58 @@
+using ChatApp.Domain.Enums;
+using ChatApp.Domain.Repositories;
+using ChatApp.Domain.Factories;
+using ChatApp.Presentation.Helpers;
+using ChatApp.Data.Entities.Models;
+namespace ChatApp.Presentation.Actions.UserActions;
+
+public static class UserActions
+{
+    private static UsersRepository _userRepository = RepositoryFactory.Create<UsersRepository>();
+    public static void RegisterUser(string email, string userName, string password)
+    {
+         Users newUser = new(email, password, userName);
+         var responseResult = _userRepository.Add(newUser);
+   
+         if(responseResult == ResponseResultType.Success)
+            Console.WriteLine($"User {userName} registered successfully!\n");
+         else
+            Console.WriteLine("Failed to register user, no changes saved!");
+         Reader.ReadKeyToContinue();
+    }
+
+    public static void DeleteUser(int id)
+    {
+        var responseResult = _userRepository.Delete(id);
+        if(responseResult == ResponseResultType.Success)
+            Console.WriteLine($"User with id {id} deleted successfully!\n");
+        else
+            Console.WriteLine("Failed to delete user, no changes saved!");
+
+        Reader.ReadKeyToContinue();
+    }
+
+    public static Users? GetUserByEmailForRegister(string email)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if(user is not null)
+            Console.WriteLine($"User with email {email} already exists!");
+        return user!;
+    }
+
+    public static Users? GetUserByEmailForLogin(string email)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if(user is null)
+            Console.WriteLine($"User with email {email} does not exist!");
+        return user!;
+    }
+    public static void UpdateUser(Users user, int id)
+    {
+        var responseResult = _userRepository.Update(user, id);
+        if (responseResult == ResponseResultType.Success)
+            Console.WriteLine($"Changes saved!\n");
+        else
+            Console.WriteLine("Failed to update, no changes saved!\n");
+    }
+
+}
