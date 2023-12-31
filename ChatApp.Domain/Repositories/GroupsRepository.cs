a8c72b0ba64cdd81b47696b6a607ff3c27c95862
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-31 15:13:21 +0100

    added group and groupusers repos in domain

--- ChatApp.Domain/Repositories/GroupsRepository.cs
+++ ChatApp.Domain/Repositories/GroupsRepository.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChatApp.Data.Entities;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Domain.Enums;
 
-namespace ChatApp.Domain.Repositories
+namespace ChatApp.Domain.Repositories;
+
+public class GroupsRepository : BaseRepository
 {
-    internal class GroupActions
+    public GroupsRepository(ChatAppDbContext dbContext) : base(dbContext)
     {
     }
+
+    public ResponseResultType AddGroup(Groups group)
+    {
+        DbContext.Groups.Add(group);
+
+        return SaveChanges();
+    }
+
+    public ResponseResultType AddUserToGroup(int userId, int groupId)
+    {
+        var group = DbContext.Groups.Find(groupId);
+        if (group is null)
+        {
+            return ResponseResultType.NotFound;
+        }
+
+        var groupUser = new GroupUsers
+        {
+            UserId = userId,
+            GroupId = groupId
+        };
+        DbContext.GroupUsers.Add(groupUser);
+
+        return SaveChanges();
+    }
+
+
 }
+
+
