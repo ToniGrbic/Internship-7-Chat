015792d224891c6b344a7f473393d21666f0e5c3
ToniGrbic <tonigrbic.5@gmail.com>
2024-01-01 20:27:12 +0100

    fourth migration

---
+++ ChatApp.Data/Migrations/20240101172441_FourthMigrationTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Data.Migrations
+{
+    public partial class FourthMigrationTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MessageDate",
+                table: "GroupMessages",
+                newName: "SentDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SentDate",
+                table: "GroupMessages",
+                newName: "MessageDate");
+        }
+    }
+}
