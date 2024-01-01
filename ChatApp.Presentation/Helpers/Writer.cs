9783be38b2601e656250426c5b3fa475be9ebf4c
ToniGrbic <tonigrbic.5@gmail.com>
2024-01-02 00:34:01 +0100

    AddLineBreaksToMessage method

--- ChatApp.Presentation/Helpers/Writer.cs
+++ ChatApp.Presentation/Helpers/Writer.cs
@@ -12,4 +12,39 @@ public static class Writer
             $"Is logged in: {user.IsLogged}\n"
         );
     }
+
+    public static string AddLineBreaksToMessage(string message, int maxCharactersPerLine)
+    {
+        string[] words = message.Split(' ');
+
+        int currentLineLength = 0;
+        string formattedMessage = "";
+
+        foreach (string word in words)
+        {
+            if (currentLineLength + word.Length + 1 > maxCharactersPerLine)
+            {
+                formattedMessage += Environment.NewLine;
+                currentLineLength = 0;
+            }
+            
+            if (word.Length > maxCharactersPerLine)
+            {
+                // If the word itself is longer than the maxCharactersPerLine
+                // Insert a newline character within the word at the specified index
+                int index = maxCharactersPerLine;
+                string newWord = word.Insert(index, Environment.NewLine);
+                formattedMessage += newWord;
+            }
+            else
+            {
+                // Add the word normally
+                formattedMessage += word + " ";
+                currentLineLength += word.Length + 1;
+            }
+        }
+
+        formattedMessage = formattedMessage.TrimEnd();
+        return formattedMessage;
+    }
 }
