2eef4c8c86364140255014842c26e7f7d9c97cb5
ToniGrbic <tonigrbic.5@gmail.com>
2024-01-01 20:26:18 +0100

    groupMessages actions and repository methods

---
+++ ChatApp.Domain/Repositories/GroupMessagesRepository.cs
@@ -0,0 +1,36 @@
+﻿
+using ChatApp.Data.Entities;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Domain.Repositories
+{
+    public class GroupMessagesRepository : BaseRepository 
+    {
+        public GroupMessagesRepository(ChatAppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public ResponseResultType Add(GroupMessages groupMessage)
+        {
+            DbContext.GroupMessages.Add(groupMessage);
+
+            return SaveChanges();
+        }
+
+        public List<GroupMessages>? GetAll(int groupId)
+        {
+            var groupMessages = DbContext.GroupMessages
+                                    .Include(gm => gm.SenderUser)
+                                    .Where(gm => gm.GroupID == groupId)
+                                    .OrderBy(gm => gm.SentDate)
+                                    .ToList();
+
+            if (groupMessages == null)
+                return null;
+
+            return groupMessages;
+        }
+    }
+}
---
+++ ChatApp.Presentation/Actions/GroupMessagesActions.cs
@@ -0,0 +1,39 @@
+﻿
+using ChatApp.Domain.Factories;
+using ChatApp.Domain.Repositories;
+using ChatApp.Domain.Enums;
+using ChatApp.Data.Entities.Models;
+using ChatApp.Presentation.Helpers;
+
+namespace ChatApp.Presentation.Actions
+{
+    public class GroupMessagesActions
+    {
+        private static GroupMessagesRepository _groupMessagesRepository = RepositoryFactory.Create<GroupMessagesRepository>();
+
+        public static void CreateAndAddGroupMessage(int userId, int groupId, string messageText)
+        {
+            var newGroupMessage = new GroupMessages(groupId, userId, messageText);
+            var responseResult = _groupMessagesRepository.Add(newGroupMessage);
+            if (responseResult == ResponseResultType.Success)
+                Console.WriteLine($"Message sent successfully!\n");
+            else
+            {
+                Console.WriteLine("Failed to send message, no changes saved!\n");
+                Reader.ReadKeyToContinue();
+            }
+                
+        }
+
+        public static List<GroupMessages>? GetAllGroupMessages(int groupId)
+        {
+            var groupMessages = _groupMessagesRepository.GetAll(groupId);
+            if (groupMessages == null)
+            {
+                Console.WriteLine("No messages!");
+                return null;
+            }
+            return groupMessages!;
+        }
+    }
+}
