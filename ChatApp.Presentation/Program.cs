bb5c5bc436a104cefd0de64a66ae248a11fd91db
ToniGrbic <tonigrbic.5@gmail.com>
2024-01-02 02:13:07 +0100

    added navigation instructions

--- ChatApp.Presentation/Program.cs
+++ ChatApp.Presentation/Program.cs
@@ -2,15 +2,22 @@
 using ChatApp.Presentation.Views.Auth;
 using ChatApp.Presentation.Views.Menus;
 using ChatApp.Presentation.Helpers;
-Console.WriteLine("Welcome to ChatApp!\n");
+
+Console.WriteLine(
+    "Welcome to ChatApp!\n" +
+    "--------------------------------------------------\n" +
+    "Navigate the menu options using up/down arrow keys\n" +
+    "and press enter to select an option.\n"
+);
 
 Reader.ReadKeyToContinue();
+
 var options = new List<(string, Action)>() {  
     ( "Exit", () => Environment.Exit(0) ),
     ( "Login", () => new Login() ), 
     ( "Register", () => new Register() )
-   
 };
+
 var menu = new Menu("Start Menu", options);
 menu.Start();
 
